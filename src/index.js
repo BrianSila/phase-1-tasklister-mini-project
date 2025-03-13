77488adce02bdb67d254341351a36836b3892fe6
Brian <brian.sila1@student.moringaschool.com>
2025-03-13 11:59:06 +0300

    phase-1-tasklister-mini-project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+  const taskList = document.getElementById('tasks');
+
+  form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    const newTask = document.createElement('li');
+    newTask.textContent = event.target.elements['new-task-description'].value;
+    taskList.appendChild(newTask);
+    form.reset();
+  });
 });
